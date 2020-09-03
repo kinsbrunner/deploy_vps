362395497701b0df73c36d10dd4ef731c36d8e91
Alex Kinsbrunner <kinsbrunner@gmail.com>
2020-09-03 16:43:27 -0300

    Update vps_install.sh
    Fixing code in order to avoid user interaction and removing useless stuff

--- vps_install.sh
+++ vps_install.sh
@@ -27,44 +27,18 @@ pip install colored
 sudo systemctl start snap
 echo 'PATH=$PATH:/snap/bin' >> ~/.bashrc
 
-##install rust
-#echo "Installing rust"
-#curl https://sh.rustup.rs -sSf | sh
-#"Done"
-
-#install go
-if [[ -z "$GOPATH" ]];then
-echo "It looks like go is not installed, would you like to install it now"
-PS3="Please select an option : "
-choices=("yes" "no")
-select choice in "${choices[@]}"; do
-        case $choice in
-                yes)
-
-                        echo "Installing Golang"
-                        wget https://dl.google.com/go/go1.13.4.linux-amd64.tar.gz
-                        sudo tar -xvf go1.13.4.linux-amd64.tar.gz
-                        sudo mv go /usr/local
-                        export GOROOT=/usr/local/go
-                        export GOPATH=$HOME/go
-                        export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
-                        echo 'export GOROOT=/usr/local/go' >> ~/.bashrc
-                        echo 'export GOPATH=$HOME/go'   >> ~/.bashrc
-                        echo 'export PATH=$GOPATH/bin:$GOROOT/bin:$PATH' >> ~/.bashrc
-                        source ~/.bashrc
-                        sleep 1
-                        break
-                        ;;
-                no)
-                        echo "Please install go and rerun this script"
-                        echo "Aborting installation..."
-                        exit 1
-                        ;;
-        esac
-done
-fi
-
-
+echo "Installing Golang"
+wget https://dl.google.com/go/go1.13.4.linux-amd64.tar.gz
+sudo tar -xvf go1.13.4.linux-amd64.tar.gz
+sudo mv go /usr/local
+export GOROOT=/usr/local/go
+export GOPATH=$HOME/go
+export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
+echo 'export GOROOT=/usr/local/go' >> ~/.bashrc
+echo 'export GOPATH=$HOME/go'   >> ~/.bashrc
+echo 'export PATH=$GOPATH/bin:$GOROOT/bin:$PATH' >> ~/.bashrc
+source ~/.bashrc
+sleep 1
 
 #create a tools folder in ~/
 mkdir ~/tools
@@ -199,7 +173,8 @@ cd ~/tools
 git clone https://github.com/swisskyrepo/PayloadsAllTheThings.git
 
 #Here is where Go executables are, to avoid needing to write full path before executing
-echo 'PATH=$PATH:/root/go/bin' >> ~/.bashrc
+#echo 'PATH=$PATH:/root/go/bin' >> ~/.bashrc
+export PATH=$PATH:/root/go/bin
 
 echo -e "\n\n\n\n\n\n\n\n\n\n\nDone! All tools are set up in ~/tools"
 cd ~/tools
